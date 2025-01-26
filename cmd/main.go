ff8af1ca7c135c5566d325e2345caa7460b5c305
MacBook <21mechanic21@gmail.com>
2025-01-26 15:36:33 +0200

    add logger file

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// TODO - init config (viper, cleanenv)
+	// TODO - init logger (slog)
+	// TODO - init storage (postgres, redis)
+	// TODO - init router
+	// TODO - init server
+}
--- main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	// Call the function
-	fmt.Println("hello world")
-}
